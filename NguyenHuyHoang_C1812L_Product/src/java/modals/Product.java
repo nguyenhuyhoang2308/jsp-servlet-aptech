9aa8e8c7cf5d35d44827ad7302ee0ca61a632d9c
@hoangnh3 <hoangnh@langtech.vn>
2020-08-19 18:08:20 +0700

    Bai tap product

---
+++ NguyenHuyHoang_C1812L_Product/src/java/modals/Product.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modals;
+
+/**
+ *
+ * @author Asus
+ */
+public class Product {
+    String name, category, price, description;
+
+    public Product() {
+    }
+
+    public Product(String name, String category, String price, String description) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
